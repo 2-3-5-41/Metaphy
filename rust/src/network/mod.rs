3d189d33ef8d6bdc8424cddffb7e39573c408e73
2-3-5-41 <93025618+2-3-5-41@users.noreply.github.com>
2024-03-14 20:41:08 -0400

    Figuring out how to use mpsc.

--- rust/src/network/mod.rs
+++ rust/src/network/mod.rs
@@ -1,32 +1,59 @@
 use godot::prelude::*;
-use tokio::{io::AsyncWriteExt, net::TcpStream, runtime};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 #[derive(GodotClass)]
 #[class(base=Node)]
-struct NetowrkEventServer {
+pub struct TokioThreadCommunicationTest {
     base: Base<Node>,
+    sender: Option<Sender<String>>,
+    receiver: Option<Receiver<String>>,
 }
 
 #[godot_api]
-impl INode for NetowrkEventServer {
+impl INode for TokioThreadCommunicationTest {
     fn init(base: Base<Node>) -> Self {
-        Self { base }
+        Self { base, sender: None, receiver: None }
     }
-
     fn ready(&mut self) {
-        let rt = runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .expect("Failed to create Tokio Runtime!");
+        let (sa, mut ra) = channel::<String>(32);
+        let (sb, rb) = channel::<String>(32);
 
-        rt.spawn(async {
-            match TcpStream::connect("127.0.0.1:23541").await {
-                Ok(mut stream) => match stream.write_all(b"Hello, world!").await {
-                    Ok(_) => godot_print!("Attempted to write message to TCP stream..."),
-                    Err(e) => panic!("{e:?}"),
-                },
-                Err(e) => panic!("{e:?}"),
+        let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+
+        rt.spawn(async move {
+            loop {
+                match ra.recv().await {
+                    Some(message) => {
+                        println!("{message}");
+                        sb.send("Pong".into()).await.unwrap();
+                    },
+                    None => continue,
+                }
             }
         });
+
+        self.sender = Some(sa);
+        self.receiver = Some(rb)
+    }
+    fn physics_process(&mut self, _delat: f64) {
+        let sender = self.sender.as_mut().unwrap();
+        let receiver = self.receiver.as_mut().unwrap();
+
+        match receiver.try_recv() {
+            Ok(message) => {
+                println!("Got -> {message}");
+                match sender.try_send("Sending -> Ping".into()) {
+                    Ok(_) => (),
+                    Err(e) => println!("{e:?}"),
+                };
+            },
+            Err(err) => match err {
+                tokio::sync::mpsc::error::TryRecvError::Empty => match sender.try_send("Got nothing, sending -> Ping".into()) {
+                    Ok(_) => (),
+                    Err(e) => godot_error!("{e:?}"),
+                },
+                tokio::sync::mpsc::error::TryRecvError::Disconnected => godot_error!("Channels diconnected!"),
+            },
+        }
     }
 }
