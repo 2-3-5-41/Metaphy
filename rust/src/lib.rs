353925471640747f7478b78cb0af01180f309de1
2-3-5-41 <93025618+2-3-5-41@users.noreply.github.com>
2024-03-15 10:22:02 -0400

    minor change

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,20 +6,20 @@ struct MyExtension;
 
 #[gdextension]
 unsafe impl ExtensionLibrary for MyExtension {
-    fn on_level_init(level: InitLevel) {
-        let mut engine = Engine::singleton();
-        match level {
-            InitLevel::Scene => (),
-            _ => (),
-        }
-    }
+    // fn on_level_init(level: InitLevel) {
+    //     let mut engine = Engine::singleton();
+    //     match level {
+    //         InitLevel::Scene => (),
+    //         _ => (),
+    //     }
+    // }
 
-    fn on_level_deinit(level: InitLevel) {
-        let mut engine = Engine::singleton();
+    // fn on_level_deinit(level: InitLevel) {
+    //     let mut engine = Engine::singleton();
 
-        match level {
-            InitLevel::Scene => (),
-            _ => (),
-        }
-    }
+    //     match level {
+    //         InitLevel::Scene => (),
+    //         _ => (),
+    //     }
+    // }
 }
